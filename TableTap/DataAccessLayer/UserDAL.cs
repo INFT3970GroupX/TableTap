da366481be2cf958b078cedfeda2605a640929d3
bm0858 <xemni999@gmail.com>
2018-09-03 14:42:27 +1000

    Dal, bl

---
+++ TableTap/DataAccessLayer/UserDAL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using TableTap.Models;
+
+namespace TableTap.DataAccessLayer
+{
+    public class UserDAL
+    {
+        public static List<UserModel> LoadUsersList()
+        {
+            List<UserModel> users = new List<UserModel>();
+
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+
+            using (conn)
+            {
+                conn.Open();
+
+                using (SqlCommand command = new SqlCommand(
+                    "SELECT * FROM tblUser" ,
+                    conn))
+                {
+                    SqlDataReader dr = command.ExecuteReader();
+                    UserModel user;
+                    while (dr.Read())
+                    {
+                        user = new UserModel();
+                        user.UserID = Convert.ToInt32(dr["userID"]);
+                        user.Email = dr["emailAddress"].ToString();
+                        user.Password = dr["passcode"].ToString();
+                        user.FirstName = dr["firstName"].ToString();
+                        user.LastName = dr["lastName"].ToString();
+                        user.AdminPermission = Convert.ToByte(dr["adminPermission"]);
+
+                        users.Add(user);
+                    }
+                    dr.Close();
+                }
+                conn.Close();
+            }
+
+            return users;
+        }
+
+    }
+}
---
+++ TableTap/UL/BeauTestPage.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TableTap.BusinessLayer;
+using TableTap.Models;
+
+namespace TableTap.UL
+{
+    public partial class BeauTestPage : System.Web.UI.Page
+    {
+
+        List<UserModel> users = new List<UserModel>();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+        protected void TestButton_Click(object sender, EventArgs e)
+        {
+            users = UserBL.FillUsersList();
+
+            
+        }
+    }
+}
