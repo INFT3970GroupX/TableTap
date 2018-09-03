12da8bfba0d42cde0b83356743c4084aadc67e15
bm0858 <xemni999@gmail.com>
2018-09-03 13:50:44 +1000

    User and Table models

---
+++ TableTap/Models/TableModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TableTap.Models
+{
+    public class TableModel
+    {
+        public int ReservationID { get; set; }
+
+        public int UserID { get; set; }
+
+        public int TableID { get; set; }
+
+        public DateTime ReservationStartTime { get; set; }
+
+        public DateTime ReservationEndTime { get; set; }
+
+        public string GroupName { get; set; }
+
+        //public TableModel()
+        //{
+
+        //}
+
+        public TableModel(int resid, int uid, int tid, DateTime resstart, DateTime resend, string group)
+        {
+            this.ReservationID = resid;
+            this.UserID = uid;
+            this.TableID = tid;
+            this.ReservationStartTime = resstart;
+            this.ReservationEndTime = resend;
+            this.GroupName = group;  
+        }
+    }
+}
---
+++ TableTap/Models/UserModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TableTap.Models
+{
+    public class UserModel
+    {
+        public int UserID { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int AdminPermission { get; set; }
+
+        //public UserModel()
+        //{
+
+        //}
+
+        public UserModel(int id, string email, string psw, string fname, string lname, int admperm)
+        {
+            this.UserID = id;
+            this.Email = email;
+            this.Password = psw;
+            this.FirstName = fname;
+            this.LastName = lname;
+            this.AdminPermission = admperm;
+        }
+    }
+}
+
+
+        /*
+         * userID			INT IDENTITY(100001,1) PRIMARY KEY,
+	       emailAddress	    NVARCHAR(100) NOT NULL,
+	       passcode		    NVARCHAR(30) NOT NULL,
+	       firstName		NVARCHAR(40),
+	       lastName		    NVARCHAR(40),
+	       adminPermission	BIT NOT NULL DEFAULT 0
+         * 
+         * /
+
+ 
+ 
