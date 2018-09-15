ab93174bf4e0a2d06242e2721a2881298f88f354
Jerry-T-Kane <42736118+Jerry-T-Kane@users.noreply.github.com>
2018-09-15 11:21:02 +1000

    comment

--- TableTap/BusinessLayer/Classes/User.cs
+++ TableTap/BusinessLayer/Classes/User.cs
@@ -4,6 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+/// <summary>
+/// OBSELETE REFER TO USERBL IN DataAccessLayer/Classes
+/// </summary>
+
+
 namespace TableTap.BusinessLayer.Classes
 {
     //Instantiazation Method Class. Creates User with AddUser method.
